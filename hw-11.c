c21f70b551802290b7eb64c5d5b0da67663e0dfa
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-10-26 15:06:26 -0400

    making prints more clear

--- hw-11.c
+++ hw-11.c
@@ -6,20 +6,23 @@
 int main(){
     int random[10];
     FILE *file = open("/dev/random", O_RDONLY, 0);
-    printf("%s", strerror(read(file, random, 40)));
+    read(file, random, 40);
+    printf("Generating random numbers: \n");
     int i;
     for (i = 0; i < 10; i++)
     {
-        printf("%d\n", random[i]);
+        printf("\trandom %d: %d\n", i+1, random[i]);
     }
+    printf("Writing numbers to file\n");
     remove("randomresult.bin");
-    FILE *createFile = open("randomresult.bin", O_WRONLY | O_CREAT, 0);
-    printf("%s", strerror(write(createFile, random, 40)));
-    createFile = open("randomresult.bin", O_RDONLY, 0);
-    read(createFile, random, 40);
-    printf("new data read");
+    file = open("randomresult.bin", O_WRONLY | O_CREAT, 0);
+    write(file, random, 40);
+    file = open("randomresult.bin", O_RDONLY, 0);
+    read(file, random, 40);
+    printf("Reading numbers from file\n");
+    printf("Verifying that written values are the same: \n");
     for (i = 0; i < 10; i++)
     {
-        printf("%d\n", random[i]);
+        printf("\trandom %d: %d\n", i+1, random[i]);
     }
 }
