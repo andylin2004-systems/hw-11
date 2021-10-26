d848a1ef4e671a9b698860628ecbc91d1ae4c1dc
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-10-26 14:07:57 -0400

    fix issue with not being able to write correctly and using wrong open

--- hw-11.c
+++ hw-11.c
@@ -5,19 +5,25 @@
 
 int * readRandomFile(){
     int random[10];
-    FILE *file = fopen("/dev/random", "r");
-    read(file, random, 40);
+    FILE *file = open("/dev/random", O_RDONLY, 0);
+    printf("%s", strerror(read(file, random, 40)));
     return random;
 }
 
 int main(){
     int i;
     int *random = readRandomFile();
-    char buffer[] = "";
     for (i = 0; i < 10; i++)
     {
         printf("%d\n", random[i]);
     }
-    FILE *createFile = fopen("randomresult", "a");
-    write(createFile, random, 40);
+    FILE *createFile = open("randomresult", O_RDWR | O_CREAT, 0);
+    printf("%s", strerror(write(createFile, random, 40)));
+    int readRandom[10];
+    read(createFile, readRandom, 40);
+    printf("new data");
+    for (i = 0; i < 10; i++)
+    {
+        printf("%d\n", readRandom[i]);
+    }
 }
