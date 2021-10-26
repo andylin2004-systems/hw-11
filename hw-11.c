66c770b00bf6f18eb2de36ac7e1d4d9308f074e8
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-10-26 15:19:22 -0400

    error checking, clear random out values and fix perms

--- hw-11.c
+++ hw-11.c
@@ -5,8 +5,11 @@
 
 int main(){
     int random[10];
-    FILE *file = open("/dev/random", O_RDONLY, 0);
-    read(file, random, 40);
+    FILE *file = open("/dev/random", O_RDONLY, 0644);
+    int error = read(file, random, 40);
+    if (error == -1){
+        printf("error %d when reading\n", error);
+    }
     printf("Generating random numbers: \n");
     int i;
     for (i = 0; i < 10; i++)
@@ -15,10 +18,20 @@ int main(){
     }
     printf("Writing numbers to file\n");
     remove("randomresult.bin");
-    file = open("randomresult.bin", O_WRONLY | O_CREAT, 0);
-    write(file, random, 40);
-    file = open("randomresult.bin", O_RDONLY, 0);
-    read(file, random, 40);
+    file = open("randomresult.bin", O_WRONLY | O_CREAT, 0644);
+    error = write(file, random, 40);
+    if (error == -1){
+        printf("error %d when writing\n", error);
+    }
+    file = open("randomresult.bin", O_RDONLY, 0644);
+    for (i = 0; i < 10; i++)
+    {
+        random[i] = NULL;
+    }
+    error = read(file, random, 40);
+    if (error == -1){
+        printf("error %d when reading\n", error);
+    }
     printf("Reading numbers from file\n");
     printf("Verifying that written values are the same: \n");
     for (i = 0; i < 10; i++)
