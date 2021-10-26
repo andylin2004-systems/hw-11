bd95af92d736142bab58fcdb6027691b54c389fa
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-10-26 14:59:55 -0400

    combined funcs together and fixed issue where results didnt match, simplified codebase

--- hw-11.c
+++ hw-11.c
@@ -3,27 +3,23 @@
 #include <stdio.h>
 #include <string.h>
 
-int * readRandomFile(){
+int main(){
     int random[10];
     FILE *file = open("/dev/random", O_RDONLY, 0);
     printf("%s", strerror(read(file, random, 40)));
-    return random;
-}
-
-int main(){
     int i;
-    int *random = readRandomFile();
     for (i = 0; i < 10; i++)
     {
         printf("%d\n", random[i]);
     }
-    FILE *createFile = open("randomresult", O_RDWR | O_CREAT, 0);
+    remove("randomresult.bin");
+    FILE *createFile = open("randomresult.bin", O_WRONLY | O_CREAT, 0);
     printf("%s", strerror(write(createFile, random, 40)));
-    int readRandom[10];
-    read(createFile, readRandom, 40);
-    printf("new data");
+    createFile = open("randomresult.bin", O_RDONLY, 0);
+    read(createFile, random, 40);
+    printf("new data read");
     for (i = 0; i < 10; i++)
     {
-        printf("%d\n", readRandom[i]);
+        printf("%d\n", random[i]);
     }
 }
